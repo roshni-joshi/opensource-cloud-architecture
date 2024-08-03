85bd99bc5303c3861a00e2bfa67b96a0098d327e
Roshni Joshi <rs888392@dal.ca>
2024-08-03 20:22:21 -0300

    Updated urls

--- client/src/api/service_api.js
+++ client/src/api/service_api.js
@@ -3,20 +3,23 @@ import axios from 'axios';
 const {
     REACT_APP_COMMON_DATA_SERVICE_PORT,
     REACT_APP_AUTHENTICATION_SERVICE_PORT,
-    REACT_APP_SEARCH_SUGGESTION_SERVICE_PORT,
-    REACT_APP_COMMON_DATA_SERVICE_URL,
-    REACT_APP_AUTHENTICATION_SERVICE_URL,
-    REACT_APP_SEARCH_SUGGESTION_SERVICE_URL
+    REACT_APP_SEARCH_SUGGESTION_SERVICE_PORT
 } = process.env
 
+export const getBaseURL = () => {
+    const { hostname, protocol } = window.location;
+    const url = `${protocol}//${hostname}`;
+    return url;
+}  
+
 export const authServiceAPI = axios.create({
-    baseURL: REACT_APP_AUTHENTICATION_SERVICE_URL || `http://localhost:${REACT_APP_AUTHENTICATION_SERVICE_PORT}`
+    baseURL: getBaseURL() + `:${REACT_APP_AUTHENTICATION_SERVICE_PORT}`
 })
 
 export const commonServiceAPI = axios.create({
-    baseURL: REACT_APP_COMMON_DATA_SERVICE_URL || `http://localhost:${REACT_APP_COMMON_DATA_SERVICE_PORT}`
+    baseURL: getBaseURL() + `:${REACT_APP_COMMON_DATA_SERVICE_PORT}`
 })
 
 export const searchSuggestionServiceAPI = axios.create({
-    baseURL: REACT_APP_SEARCH_SUGGESTION_SERVICE_URL || `http://localhost:${REACT_APP_SEARCH_SUGGESTION_SERVICE_PORT}`
+    baseURL: getBaseURL() + `:${REACT_APP_SEARCH_SUGGESTION_SERVICE_PORT}`
 })
