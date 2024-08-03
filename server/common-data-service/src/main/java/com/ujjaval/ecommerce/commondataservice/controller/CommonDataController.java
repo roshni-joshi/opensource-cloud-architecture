2496502d1de4eecf8ae9ae4cb2b63d9fc734aa59
Roshni Joshi <rs888392@dal.ca>
2024-08-03 18:51:50 -0300

    Updated cors

--- server/common-data-service/src/main/java/com/ujjaval/ecommerce/commondataservice/controller/CommonDataController.java
+++ server/common-data-service/src/main/java/com/ujjaval/ecommerce/commondataservice/controller/CommonDataController.java
@@ -12,6 +12,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,6 +21,7 @@ import java.util.HashMap;
 import java.util.Objects;
 
 @RestController
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 public class CommonDataController {
 
     @Autowired
