63ba7d462eca20ea30658cfab8556622d633dd27
Roshni Joshi <rs888392@dal.ca>
2024-08-04 16:22:33 -0300

    Updated env variable

--- client/src/api/service_api.js
+++ client/src/api/service_api.js
@@ -3,7 +3,8 @@ import axios from 'axios';
 const {
     REACT_APP_COMMON_DATA_SERVICE_PORT,
     REACT_APP_AUTHENTICATION_SERVICE_PORT,
-    REACT_APP_SEARCH_SUGGESTION_SERVICE_PORT
+    REACT_APP_SEARCH_SUGGESTION_SERVICE_PORT,
+    REACT_APP_INSTANCE_IP
 } = process.env
 
 // export const getBaseURL = () => {
@@ -14,13 +15,13 @@ const {
 // }  
 
 export const authServiceAPI = axios.create({
-    baseURL: `http://csci5411-lb-999554601.us-east-1.elb.amazonaws.com:${REACT_APP_AUTHENTICATION_SERVICE_PORT}`
+    baseURL: `http://${REACT_APP_INSTANCE_IP}:${REACT_APP_AUTHENTICATION_SERVICE_PORT}`
 })
 
 export const commonServiceAPI = axios.create({
-    baseURL: `http://csci5411-lb-999554601.us-east-1.elb.amazonaws.com:${REACT_APP_COMMON_DATA_SERVICE_PORT}`
+    baseURL: `http://${REACT_APP_INSTANCE_IP}:${REACT_APP_COMMON_DATA_SERVICE_PORT}`
 })
 
 export const searchSuggestionServiceAPI = axios.create({
-    baseURL: `http://csci5411-lb-999554601.us-east-1.elb.amazonaws.com:${REACT_APP_SEARCH_SUGGESTION_SERVICE_PORT}`
+    baseURL: `http://${REACT_APP_INSTANCE_IP}:${REACT_APP_SEARCH_SUGGESTION_SERVICE_PORT}`
 })
