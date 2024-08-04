cd4205b8511a0787324ac86f7ed59b4df85bbb6d
Roshni Joshi <rs888392@dal.ca>
2024-08-04 12:12:13 -0300

    Updated image

--- client/src/api/service_api.js
+++ client/src/api/service_api.js
@@ -6,23 +6,21 @@ const {
     REACT_APP_SEARCH_SUGGESTION_SERVICE_PORT
 } = process.env
 
-export const getBaseURL = async () => {
-    //const { hostname, protocol } = window.location;
-    const response = await axios.get('http://169.254.169.254/latest/meta-data/local-ipv4');
-    console(response)
-    const url = "http://" + response.data;
-    console.log("giving url : " + url);
-    return url;
-}  
+// export const getBaseURL = () => {
+//     const { hostname, protocol } = window.location;
+//     const url = `${protocol}//${hostname}`;
+//     console.log("giving url new test : " + url);
+//     return url;
+// }  
 
 export const authServiceAPI = axios.create({
-    baseURL: getBaseURL() + `:${REACT_APP_AUTHENTICATION_SERVICE_PORT}`
+    baseURL: `http://csci5411-lb-999554601.us-east-1.elb.amazonaws.com:${REACT_APP_AUTHENTICATION_SERVICE_PORT}`
 })
 
 export const commonServiceAPI = axios.create({
-    baseURL: getBaseURL() + `:${REACT_APP_COMMON_DATA_SERVICE_PORT}`
+    baseURL: `http://csci5411-lb-999554601.us-east-1.elb.amazonaws.com:${REACT_APP_COMMON_DATA_SERVICE_PORT}`
 })
 
 export const searchSuggestionServiceAPI = axios.create({
-    baseURL: getBaseURL() + `:${REACT_APP_SEARCH_SUGGESTION_SERVICE_PORT}`
+    baseURL: `http://csci5411-lb-999554601.us-east-1.elb.amazonaws.com:${REACT_APP_SEARCH_SUGGESTION_SERVICE_PORT}`
 })
