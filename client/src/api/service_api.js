9d985cb5a9247b6b6996caa6915afc1f836415d2
Roshni Joshi <rs888392@dal.ca>
2024-08-04 10:31:03 -0300

    Updated ip

--- client/src/api/service_api.js
+++ client/src/api/service_api.js
@@ -3,13 +3,14 @@ import axios from 'axios';
 const {
     REACT_APP_COMMON_DATA_SERVICE_PORT,
     REACT_APP_AUTHENTICATION_SERVICE_PORT,
-    REACT_APP_SEARCH_SUGGESTION_SERVICE_PORT,
-    PRIVATE_IP
+    REACT_APP_SEARCH_SUGGESTION_SERVICE_PORT
 } = process.env
 
-export const getBaseURL = () => {
+export const getBaseURL = async () => {
     //const { hostname, protocol } = window.location;
-    const url = `http://${PRIVATE_IP}`;
+    const response = await axios.get('http://169.254.169.254/latest/meta-data/local-ipv4');
+    console(response)
+    const url = "http://" + response.data;
     console.log("giving url : " + url);
     return url;
 }  
