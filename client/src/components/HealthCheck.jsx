4ea885faa8d6d0c118ec2908a13dec9cf7d3a761
Roshni Joshi <rs888392@dal.ca>
2024-08-04 17:40:21 -0300

    Updated test

--- client/src/components/HealthCheck.jsx
+++ client/src/components/HealthCheck.jsx
@@ -1,9 +1,18 @@
-import React, { useEffect } from 'react'
+import React, { useEffect } from 'react';
+import axios from "axios";
 
 function HealthCheck() {
 
   useEffect(() => {
     console.log(process.env.REACT_APP_INSTANCE_IP);
+    axios.get("http://csci5411-lb-999554601.us-east-1.elb.amazonaws.com:10000/default-search-suggestion")
+      .then(data => {
+        JSON.stringify(data)
+        console.log("heath data:" + data)
+      })
+      .catch(error => {
+        console.log("health error : " + error)
+      })
   }, []);
 
   return (
